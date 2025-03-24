09b378d863e99eb34c83c897a805ee7048edf0c7
MaximeRogues <maximerogues1997@gmail.com>
2025-03-24 21:15:42 +0100

    Utilisation de Spring Security (fin)

--- lab/42-security-rest/src/main/java/accounts/RestWsApplication.java
+++ lab/42-security-rest/src/main/java/accounts/RestWsApplication.java
@@ -6,15 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
 
-// TODO-00: In this lab, you are going to exercise the following:
-// - Observing the default security behavior
-// - Configuring authorization based on roles
-// - Configuring authentication using in-memory storage
-// - Configuring method-level security
-// - Adding custom UserDetailsService
-// - Adding custom AuthenticationProvider
-// - Writing test code for security
-
 @SpringBootApplication
 @Import(RestSecurityConfig.class)
 @EntityScan("rewards.internal")
@@ -25,11 +16,3 @@ public class RestWsApplication {
     }
 
 }
-
-// TODO-19 (Optional): Verify that the newly added custom AuthenticationProvider works
-// - Re-run this application
-// - Using Chrome Incognito browser, access
-//   http://localhost:8080/accounts/0
-// - Enter "spring"/"spring" and verify accounts data
-// - If you want to use "curl", use
-//   curl -i -u spring:spring http://localhost:8080/accounts/0
